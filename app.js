71d91c910a45fa466c087db4deffd93c96938d3a
Albertina75 <145276835+Albertina75@users.noreply.github.com>
2023-10-19 13:42:45 +0200

    y se hizo la luz

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // Buena suerte ! Espero que se te encianda la "bombilla"!
+
+// obtengo el elemento de la ombilla
+
+
+var bulb = document.getElementById('bulb');
+
+//Agrego un evento clic al elemento de la bombilla
+
+bulb.addEventListener('click', function() {
+
+
+    //alterno entre las classes 'on' y 'off'
+if (bulb.classList.contains('bulb-on')) {
+    bulb.classList.remove('bulb-on');
+    bulb.classList.add('bulb-off');
+}
+else {
+    bulb.classList.remove('bulb-off');
+    bulb.classList.add('bulb-on');
+}
+
+});
